3a4de9c0279f4543feac4179bf47872be4b0de7c
Mark Roth <mroth@Blanche.(none)>
2010-10-29 12:14:08 -0700

    adding automated run and test script

---
+++ pkgs/apps/fluidanimate/scripts/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -rf output/$1.out.fluid
+../inst/amd64-linux.gcc-$1/bin/fluidanimate $2 $3 ../inputs/in_500K.fluid output/$1.out.fluid
+../obj/amd64-linux.gcc-tbb/cmp output/serial.out.fluid output/$1.out.fluid
